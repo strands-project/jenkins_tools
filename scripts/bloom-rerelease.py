4796300949381e56b9f00600f4e83b8a2b5588d7
Marc Hanheide <marc@hanheide.net>
2014-11-20 19:13:05 +0000

    implemented support for proper release branches

--- scripts/bloom-rerelease.py
+++ scripts/bloom-rerelease.py
@@ -8,6 +8,10 @@ import tty
 import yaml
 from tempfile import mkdtemp
 from rosdistro import get_index, get_index_url, get_distribution_file
+import subprocess
+
+from catkin_pkg.package import parse_package_string
+import rosdistro
 
 
 import subprocess
@@ -43,15 +47,67 @@ class Releaser:
         self.pretend = False
 
         try:
-            index = get_index(get_index_url())
+            self.index = get_index(get_index_url())
             self.distribution = get_distribution_file(
-                index,
+                self.index,
                 self.distro_name
             )
             self.repo = self.distribution.repositories[self.repo_name]
         except:
             print "failed to get data about repo %s in distribution %s" % (self.repo_name, self.distro_name)
             raise
+
+    def get_release_upstream_repo(self):
+        repo = self.repo
+        distro = self.distro_name
+        if not repo.release_repository:
+            raise  RuntimeError('no release repo exists')
+
+        release_repo = repo.release_repository
+        if release_repo.version is None:
+            raise  RuntimeError('no release version exists')
+
+        url = release_repo.url
+
+        prefix = 'https://github.com/'
+        if not url.startswith(prefix):
+            raise RuntimeError('wrong prefix')
+        suffix = '.git'
+        if not url.endswith(suffix):
+            raise RuntimeError('wrong suffix')
+
+        tracks_url = 'https://raw.githubusercontent.com/' + url[len(prefix):]
+        tracks_url = tracks_url[:-len(suffix)] + '/master/tracks.yaml'
+        
+
+        tracks_yaml = rosdistro.loader.load_url(tracks_url)
+        
+
+        tracks_data = yaml.load(tracks_yaml)
+        upstream_url = tracks_data['tracks'][distro]['vcs_uri']
+        # ignore local upstream path
+
+        last_version=tracks_data['tracks'][distro]['last_version']
+        release_tag=tracks_data['tracks'][distro]['release_tag'].replace(':{version}', last_version)
+
+
+        release_branch = tracks_data['tracks'][distro]['devel_branch']
+        if release_branch is None:
+            remotes = subprocess.check_output(['git', 'ls-remote', upstream_url])
+            lines = remotes.splitlines()
+            lines = [l.split('\t') for l in lines]
+            head_hash = [l[0] for l in lines if l[1] == 'HEAD'][0]
+            default_ref = [l[1] for l in lines if l[0] == head_hash and l[1] != 'HEAD'][0]
+            release_branch = default_ref.split('/')[-1]
+
+        self.upstream_url = upstream_url
+        self.release_branch = release_branch
+        return upstream_url, release_branch
+        #print release_branch, repo_name, tracks_data['tracks'][distro]
+        #tag_hash = [l[0] for l in lines if l[1] == 'refs/tags/'+release_tag][0]
+        #release_branch_hash = [l[0] for l in lines if l[1] == 'refs/heads/'+release_branch][0]
+
+
 
     def call(self, working_dir, command, pipe=None):
         print('+ cd %s && ' % working_dir + ' '.join(command))
@@ -75,9 +131,10 @@ class Releaser:
 
     def prepare_upstream(self):
         upstream_dir = mkdtemp()
-        upstream_url = self.repo.source_repository.url
+        upstream_url = self.upstream_url
         try:
-            self.call(upstream_dir, ['git', 'clone', upstream_url, upstream_dir])
+            self.call(upstream_dir, ['git', 'clone', '-b', self.release_branch, upstream_url, upstream_dir])
+
             try:
                 self.call(upstream_dir, ['catkin_generate_changelog', '-y',  '-a'])
             except:
@@ -101,6 +158,9 @@ class Releaser:
 if __name__ == "__main__":
     args = parse_options()
     r = Releaser(args.repo_name, args.rosdistro, args.track, args.bump)
+
+    r.get_release_upstream_repo()
+    
 
     r.prepare_upstream()
     if args.track is not None:
