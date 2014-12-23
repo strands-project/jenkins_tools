ad922b3bab46eec0eb4735c857004cdc83c9e61c
Marc Hanheide <marc@hanheide.de>
2014-12-23 11:24:56 +0000

    added whitelisting of specific branch for PRs

--- src/jenkins_tools/run_jenkins.py
+++ src/jenkins_tools/run_jenkins.py
@@ -107,12 +107,12 @@ def run_jenkins_periodic(jenkins_instance, ubuntu_distro, arch, job_name, email,
 
 
 # configure a job with vcs trigger
-def run_jenkins_vcs(jenkins_instance, ubuntu_distro, arch, job_name, email, vcs, uri, branch, script, script_args, user_name, matrix=None, priority=None, timeout=None, additional_publishers='', refspec='+refs/heads/*:refs/remotes/origin/*', trigger='gh'):
+def run_jenkins_vcs(jenkins_instance, ubuntu_distro, arch, job_name, email, vcs, uri, branch, script, script_args, user_name, matrix=None, priority=None, timeout=None, additional_publishers='', refspec='+refs/heads/*:refs/remotes/origin/*', trigger='gh', prbranch=''):
     jc = _get_jenkins_conf()
     params = {}
     params['EMAIL_COMMITTER'] = 'true'
     params['TRIGGER'] = jc['triggers'][trigger].replace('@(JOB_NAME)', job_name)
     params['ADDITIONAL_PUBLISHERS'] = additional_publishers
-    params['VCS'] = jc['vcs'][vcs].replace('@(URI)', uri).replace('@(BRANCH)', branch).replace('@(REFSPEC)', refspec)
+    params['VCS'] = jc['vcs'][vcs].replace('@(URI)', uri).replace('@(BRANCH)', branch).replace('@(REFSPEC)', refspec).replace('@(PRBRANCH)', prbranch)
     params['PROJECT_URL'] = uri.replace('.git','')
     _update_jenkins_job(jenkins_instance, jc, ubuntu_distro, arch, job_name, email, script, script_args, user_name, params, matrix=matrix, priority=priority, timeout=timeout)
